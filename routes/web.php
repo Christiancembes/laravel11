0a81dcd5bc9d3a9713c0df752903975f81be7e7d
christiancembes <christeifel14@gmail.com>
2024-07-01 00:48:03 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+
+
+Route::get('/', function () {
+    return view('home', [
+        'title' => 'Home Page'
+    ]);
+});
+
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About Page'
+    ]);
+});
+
+
+Route::get('/posts', function () {
+
+    return view('posts', [
+        'title' => 'Post',
+        'posts' => Post::filter(request([
+            'search',
+            'category',
+            'author'
+        ]))->latest()->get()
+    ]);
+});
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/authors/{user:username}', function (User $user) {
+    return view('posts', ['title' => count($user->posts) . ' Articles by ' .
+        $user->name, 'posts' =>  $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'title' => 'Articles in:  '  .  $category->name,
+        'posts' =>  $category->posts
+    ]);
+});
+
+
+Route::get('/contact', function () {
+    return view('contact', [
+        'title' => 'Contact Page'
+    ]);
+});
